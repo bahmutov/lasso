857dd15fbc296ce15dea63f47a4bf0b0590d19b5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-01-15 23:28:21 -0500

    added chai + mocha example

---
+++ examples/chai/foo.js
@@ -0,0 +1,9 @@
+function foo() {
+	return 'foo';
+}
+
+function notCalled() {
+	if (true) {
+		return 'this function is not called';
+	}
+}
---
+++ examples/chai/tests.js
@@ -0,0 +1,14 @@
+var expect = chai.expect;
+
+describe('foo', function(){
+  describe('sanity', function(){
+    
+    it('foo should be a function', function(){
+      expect(foo).to.be.a('function');
+    })
+
+    it('should return foo', function () {
+    	expect(foo()).to.equal('foo');
+    })
+  })
+})
