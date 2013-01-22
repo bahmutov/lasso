668cf57281cba69398949024bd593821236b22e6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-01-22 18:47:45 -0500

    refactoring

--- src/phantomjsWrapper.js
+++ src/phantomjsWrapper.js
@@ -4,6 +4,8 @@ var regexpQuote = require('regexp-quote');
 var istanbul = require('istanbul');
 var Report = istanbul.Report;
 var spawn = require('child_process').spawn;
+var _ = require('lodash');
+var untested = require('untested');
 
 function run(options) {
 	options = options || {};
@@ -13,16 +15,16 @@ function run(options) {
 	var phantomRunnerFilename = path.join(options.lassoDir, 'src', runner);
 	console.assert(fs.existsSync(phantomRunnerFilename), 'could not find phantom runner', phantomRunnerFilename);
 
-	var coverageFilename = path.join(process.cwd(), 'cover.json');
+	options.coverageFilename = path.join(process.cwd(), 'cover.json');
 
 	var separatorRegex = new RegExp(regexpQuote(path.sep), 'g');
 	var pageUrl = 'http://localhost:' + options.port + '/' + options.page.replace(separatorRegex, '/');
 	console.log('page url to load', pageUrl);
 
 	console.log('Opening in phantomjs page', pageUrl);
-	console.log('output coverage filename', coverageFilename);
+	console.log('output coverage filename', options.coverageFilename);
 
-	var phantomArguments = [phantomRunnerFilename, pageUrl, coverageFilename];
+	var phantomArguments = [phantomRunnerFilename, pageUrl, options.coverageFilename];
 	if (options.verbose) {
 		phantomArguments.push('--verbose');
 	}
@@ -37,48 +39,58 @@ function run(options) {
 	});
 
 	phantomjs.stderr.on('data', function (data) {
-		console.log('phantomjs: ' + data);
+		console.log('phantomjs error: ' + data);
 	});
 
-	phantomjs.on('exit', function (code) {
-		console.log('phantomjs process exited with code ' + code);
-		console.assert(fs.existsSync(coverageFilename), 'could not find coverage file', coverageFilename);
+	phantomjs.on('exit', _.partial(processPhantomJsResults, options));
+}
 
-		console.log('generating detailed HTML coverage pages from', coverageFilename);
-		var collector = new istanbul.Collector();
-		collector.add(JSON.parse(fs.readFileSync(coverageFilename, 'utf8')));
+function processPhantomJsResults(options, code) {
+	console.log('phantomjs process exited with code ' + code);
+	console.assert(fs.existsSync(options.coverageFilename), 
+		'could not find coverage file', options.coverageFilename);
 
-		var reportFolder = path.join(process.cwd(), 'cover');
-		var report = Report.create('html', {
-			dir: reportFolder,
-			verbose: false
-		});
-		report.writeReport(collector, true);
-		console.log('wrote html coverage reports to', reportFolder);
+	console.log('generating detailed HTML coverage pages from', options.coverageFilename);
+	var collector = new istanbul.Collector();
+	collector.add(JSON.parse(fs.readFileSync(options.coverageFilename, 'utf8')));
 
-		Report.create('text').writeReport(collector);
-		Report.create('text-summary').writeReport(collector);
+	var reportFolder = path.join(process.cwd(), 'cover');
+	var report = Report.create('html', {
+		dir: reportFolder,
+		verbose: false
+	});
+	report.writeReport(collector, true);
+	console.log('wrote html coverage reports to', reportFolder);
 
-		Report.create('text', {
-			file: 'cover.txt'
-		}).writeReport(collector);
-		console.log('saved coverage text report to', path.join(process.cwd(), 'cover.txt'));
+	Report.create('text').writeReport(collector);
+	Report.create('text-summary').writeReport(collector);
 
-		if (options.untested) {
-			var full = path.join(options.basedir, options.page);
-	  		var untested = require('untested');
+	Report.create('text', {
+		file: 'cover.txt'
+	}).writeReport(collector);
+	console.log('saved coverage text report to', path.join(process.cwd(), 'cover.txt'));
 
-	  		var coverageData = collector.getFinalCoverage();
-	  		var coverageSummary = untested.getCoverageSummary(coverageData);
-	  		console.assert(coverageSummary, 'could not get coverage summary from\n', 
-	  			JSON.stringify(coverageData, null, 2));
-	  		untested.update({
-	  			test: full, 
-	  			coverage: coverageSummary
-	  		});
-	  	}
+	if (options.untested) {
+  		var coverageData = collector.getFinalCoverage();
+  		console.assert(coverageData, 'could not get coverage data');
+  		updateUntestedDb(coverageData, options);
+  	}
 
-		process.exit(0);
+	process.exit(0);
+}
+
+function updateUntestedDb(coverageData, options)
+{
+	console.assert(options.basedir, 'missing basedir');
+	console.assert(options.page, 'missing page address');
+
+	var full = path.join(options.basedir, options.page);
+	var coverageSummary = untested.getCoverageSummary(coverageData);
+	console.assert(coverageSummary, 'could not get coverage summary from\n', 
+		JSON.stringify(coverageData, null, 2));
+	untested.update({
+		test: full, 
+		coverage: coverageSummary
 	});
 }
 
