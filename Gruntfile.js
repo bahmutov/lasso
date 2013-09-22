8a023cbff1d6ed830f0ca583b45d893cbb315080
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-09-22 16:40:36 -0400

    added grunt bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,5 +17,6 @@ module.exports = function (grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-bump');
     grunt.registerTask('default', ['jshint']);
 };
